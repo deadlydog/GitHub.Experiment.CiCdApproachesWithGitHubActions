44327f0a2d63766823be3d5f3fdabc76925e0c4a
deadlydog <deadlydog@hotmail.com>
2024-01-04 22:34:48 -0600

    Make the build pass again

--- src/pass-or-fail-build.ps1
+++ src/pass-or-fail-build.ps1
@@ -2,11 +2,9 @@
 # in the various GitHub Actions workflow results.
 
 # Set to true/false to pass/fail the build.
-[bool] $buildShouldSucceed = $false
+[bool] $buildShouldSucceed = $true
 
 if ($buildShouldSucceed -eq $false) {
 	throw "Build failed."
 }
 Write-Output "Build succeeded."
-
-# Dummy change to trigger build.
